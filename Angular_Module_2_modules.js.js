d1e670b66ce8c11d428bc5ebe96ba87b73036473
Ben Cook <benthomascook28@gmail.com>
2017-06-27 18:00:10 -0400

    added Angular module 2 Modules JS library

---
+++ Angular_Module_2_modules.js.js
@@ -0,0 +1,13 @@
+// modules are containers for code
+//dependencies go in the square bracket
+var myCountryApp = angular.module('myCountryApp', []);
+//config files run before the program
+myCountryApp.config(function(){
+
+});
+//run files run as the program fires
+myCountryApp.run(function(){
+
+});
+//controllers act as middlemen between the view and model
+myCountryApp.controller
