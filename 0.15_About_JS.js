efd070c3a59de297c497199036c8a8d96b58a557
Ben Cook <benthomascook28@gmail.com>
2017-07-14 21:20:01 -0400

    Added JS Library Module 0.15 About JS

---
+++ 0.15_About_JS.js
@@ -0,0 +1,28 @@
+//History:
+	// Became the de-facto language of the web when Java applets failed.  Gained worldwide adoption quickly.
+	// and therefore has many bad features and rough edges.  However it is powerful and expressive and has some
+	// great features such as those below:
+
+// Great Features in JavaScript:
+
+	//*loose typing: JavaScript forgives minor syntax errors by guessing at intention and
+	//acting on those guesses behind the scenes.
+
+	//*functions: Blocks of code you can re-use.  You can pass in unique arguments, which are inputs
+	// to functions to these blocks you re-use.
+
+	//*dynamic objects: objects defined as a function, which can incude functions inside of objects 
+	//, also know as methods.  Objects are class-free. We can add a new member to any object by ordinary assignment.
+	// An object can inherit members from another object.
+	
+	//*Object literal notation: Objects can be created simply by listing their components
+
+	//*Prototype inheritance: objects can inherit properties directly from other objects.  Saves time.
+
+// One huge negative:
+	//*JS depends on global variables, which can appear in unintended places in code.  We can work around this issue.
+
+// About JSLindt:
+	//*An awesome tool I've used to see whether I've formatted JSON correctly.  It identifies bad parts.
+
+	
