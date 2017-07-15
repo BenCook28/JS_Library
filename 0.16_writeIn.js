080c54e3f27d7afb91a97df273d73c9c3e207ed8
Ben Cook <benthomascook28@gmail.com>
2017-07-15 17:11:17 -0400

    Added JS Library 0.16 Pre-tag and Writeln

---
+++ 0.16_writeIn.js
@@ -0,0 +1,10 @@
+document.writeln('Hello, world!');
+
+//on the doucment (which is the HTML page)
+//there's a lowercase L after write
+
+//write('whatever you want to write') prints to the document
+
+//write() does NOT add a new line after each statement
+
+//writeln() adds a new line after each statement
