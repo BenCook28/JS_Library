e2642215c84aad9a0282d8ea6a6fbb4d4c464c0d
Ben Cook <benthomascook28@gmail.com>
2017-07-17 17:47:20 -0400

    Added Grammar Names JS Library

---
+++ 0.21_Grammar_Names.js
@@ -0,0 +1,17 @@
+//Names must start with a letter, but they can be followed by letters, digits, and underscores.
+
+//Names can't be reserved words.  You'll find a current list of reserved words here: 
+/*http://www.javascripter.net/faq/reserved.htm*/
+
+//Reserved words often change color in your text editor
+
+//Some words like undefined, NaN, and Infinity should be reserved words but aren't
+
+//Reserved words can't be used to name object properties.
+
+//Names can bue used for statements, variables, parameters, property names, operators,
+//and labels
+
+//these are valid names: helloWorld28, hi, Ben_Cook
+
+//these are not valid names: 28Ben, boolean, #Tom Cruise
