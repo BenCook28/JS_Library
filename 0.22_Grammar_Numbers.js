7342d885159f7cb2681be2e56f21f2a49f38ee45
Ben Cook <benthomascook28@gmail.com>
2017-07-18 12:53:22 -0400

    Added 0.22 Grammar Numbers JS Library

---
+++ 0.22_Grammar_Numbers.js
@@ -0,0 +1,20 @@
+//there is only one JavaScript number type.  For example, the expression below evaluates to true:
+
+(50 == 50.0);
+
+//in several other programming languages, the values would be considered different.
+
+//The number is a 64-bit floating point, which is a binary value with 64 places.
+
+//to create exponents, multiply the part before the e by 10 to the power of whatever's after the e.
+//for example:
+
+(100 == 1e2) 
+
+//to see whether a value is a number, you can use the NaN function.  NaN syntax below:
+
+isNaN("Hello World") //evaluates to true
+
+//in JS there's a Math object with several methods.
+
+//for example, Math.floor(3.14159) returns 3
