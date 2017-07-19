64ab27eb8707d65901e71002d469855d146104a7
Ben Cook <benthomascook28@gmail.com>
2017-07-19 09:28:19 -0400

    Added 0.23 Grammar Strings JS Library

---
+++ 0.23_Grammar_Strings.js
@@ -0,0 +1,24 @@
+//Single quotes and double quotes, ' ' and " ", wrapped around characters create strings.
+
+//JavaScript doesn't have character data types, but has string data types.
+
+//  \ (backslash) is the escape character which tells JavaScript to interpret characters with 
+//other meanings only as strings.  for example, \" would print " and not tell JavaScript
+//you're starting a new string.
+
+//to get the length of a string, use the format below:
+
+return "seven".length;
+
+//this'll return 5.
+
+// + concatinates strings.  For example:
+
+return "this " + "that"
+
+//returns this that.
+
+//You can use methods on strings like "lower".toUpperCase()
+return "lower".toUpperCase();
+
+//this produces LOWER.
