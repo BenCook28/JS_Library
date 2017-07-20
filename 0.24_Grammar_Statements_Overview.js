c11f328f58634015fd1ce1cd03b6379f04a8e325
Ben Cook <benthomascook28@gmail.com>
2017-07-20 17:53:46 -0400

    Added 0.24 Grammar Statements Overview JS Library

---
+++ 0.24_Grammar_Statements_Overview.js
@@ -0,0 +1,16 @@
+//a <script></script> tags in HTML deliver comilation units, which are  sets of executable
+//statements.  JavaScript considers these in a global namespace.
+
+//var defines a functions private variables, which are only available in that function's scope.
+
+// switch, while, for, and do statements use labels in their logic which interacts with the
+//'break' statement which terminates the logic.
+
+//JavaScript tends to execute statements from top to bottom, but conditional statements
+//and looping statements alter this flow.
+
+ // Important: curley braces which create blocks{} do not create new scopes in JS, so 
+ //so declare variables at the top of functions.
+
+ // In logic, JS considers the following conditional values falsy, which means JS will
+ //execute 'else' statements: false, null, undefined, the empty string '', 0, and NaN.
