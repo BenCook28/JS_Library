f1e4b4b6aa21b281ab6b4fce833c8fd4204c5b7b
Ben Cook <benthomascook28@gmail.com>
2017-07-26 09:13:46 -0400

    Added 0.29 Grammar Return, Break, and Expression Statements JS Library

---
+++ 0.29_Grammar_Return_Break_and_Expression_Statements.js
@@ -0,0 +1,15 @@
+// Return statements either cause an early return from a function,
+//or tell JS what value to return.
+//for example:
+if(a == a){
+	return "Aristotle"
+}
+//end lines cannot come between return and an expression.
+
+//Break statements cause JS to exit from loops statements and switch statements.
+//you can label breaks so you exit from the labeled statement.
+//JS doesn't allow a line end between the break and label.
+
+//Expression statements can assign values, invoke methods, or delete properties from an object.
+//We use = to assign values, += to add and assign, and -= to subtract and assign.
+// === means strict equality, == means loose equality.
