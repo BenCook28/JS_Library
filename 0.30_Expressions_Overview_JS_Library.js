df9cdcbccf84f53c2314515f2ce5d7cba6cd8680
Ben Cook <benthomascook28@gmail.com>
2017-07-27 20:02:36 -0400

    Added 0.30 Expressions Overview JS Library

---
+++ 0.30_Expressions_Overview_JS_Library.js
@@ -0,0 +1,15 @@
+// The simplest expressions are literals values. 
+
+	//Literals are fixed values that you litterally feed into your script.
+	//Literals include arrays, booleans, numbers, objects, strings, RegEx (regular expressions), and floating points.
+	//Variables can change.  They are also expressions, as are built-in-values like true, NaN, and null.
+
+	//The word new proceeds the simplist invocation expressions.  Invocation expressions call function methods.
+
+	//A refinement expression gets to a component of objects and arrays.
+	//We use .x to get object properties and [an integer] to get array components
+	//The word delete procedes the simplest refinement expressions.
+
+	//The prefix operator act on the variables after them.  For example: ++a and --a
+	//The postfix operator acts on variables before them.  For example: a++ and a--
+	//An infix oporator goes between two values.  For example: 2 + 2
