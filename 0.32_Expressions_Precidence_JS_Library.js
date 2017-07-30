396b3104fcdd43e382daf47438a16a17781c28b9
Ben Cook <benthomascook28@gmail.com>
2017-07-29 21:14:14 -0400

    Added 0.32 Expressions Precidence JS Library

---
+++ 0.32_Expressions_Precidence_JS_Library.js
@@ -0,0 +1,15 @@
+//() Parentheses change precidence order.  Otherwise:
+
+//Refinement and invocation takes precidence.  These include ., (), and []
+
+//Unary operators come next.  Univary operators only operate on one thing (operand).
+//Unary operators include new, delete, typeof, +, -, and !.
+
+//Next come multiplation, division, and modulus (%, which returns the remainer of division).
+//Next come addition and subtraction.
+//Next comes inequity: >=, <=, >, and <.
+
+//Next comes equity.  === means strict equality, which means both value and data type match.
+//== means loose equality, which means that the value matches.
+
+//Next comes the logical and &&, then comes the logical or ||, then comes ther ternary ? : .
