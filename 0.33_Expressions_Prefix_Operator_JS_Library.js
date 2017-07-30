436e53990ac5b54ba57d29578bd30ee224acb39d
Ben Cook <benthomascook28@gmail.com>
2017-07-30 14:26:32 -0400

    Added 0.33 Expressions Prefix Operator JS Library

---
+++ 0.33_Expressions_Prefix_Operator_JS_Library.js
@@ -0,0 +1,15 @@
+//Typeof returns either 'number', 'string', 'boolean', 'undefined', 'function', or 'object'.
+
+//If you use typeof on an array or null, the result will incorrectly be 'object.'
+
+//! reverses the truthiness of an operand.
+
+//+ adds if both operands are numbers and otherwise concatenates.
+
+// / can return a noninteger result even if bother operands are integers.
+
+// The && operator produces the value of its first operand if the first operand is falsy. 
+//Otherwise it produces the value of the second operand.
+
+//The paragraph on && goes against my experience, so I'll get clarification
+//tomorrow.
