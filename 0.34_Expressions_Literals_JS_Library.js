8862c9bb143c13712e5a07781dae194ba3dabffd
Ben Cook <benthomascook28@gmail.com>
2017-07-31 12:40:42 -0400

    Added 0.34 Expressions Literals JS Library

---
+++ 0.34_Expressions_Literals_JS_Library.js
@@ -0,0 +1,15 @@
+// Please let me know if you're actually reading these.
+
+//Properties can be specified as names or strings.
+
+//We must specify literal names at combile time because they're not considered variable names.
+//These name properties are expressions.
+
+// There are number literals, string literals, object literals, array literals, functions, and regex literals
+
+// Object literals have this format:
+// { property name: expression, 
+//   property string: expression
+//	}
+
+//with the last key value pair not followed by a comma.
