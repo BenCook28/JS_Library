1622e2f1c7a85aeaaf818188c48ee7cb6c696708
Ben Cook <benthomascook28@gmail.com>
2017-08-02 09:49:38 -0400

    Added 0.36 Expressions Function Literals JS Library

---
+++ 0.36_Expressions_Function_Literals.js
@@ -0,0 +1,18 @@
+//function literals follow the following pattern:
+// var variableName = function(Argument List){
+	//Function Body
+// };
+
+//Function literal parameters can be seperated by commas if there are more than one.
+
+//The function body includes variable definitions and statements.  Statements contain 
+//executable code.  The parameters act as variables initialized by the invocation arguments.
+
+//A function can have a name which can be used recursively.  Recursion is when something calls itself.
+//A dream within a dream within a dream is an example of recursion.
+
+//Here's an example:
+
+var x = function(input){
+	console.log(input);
+}
