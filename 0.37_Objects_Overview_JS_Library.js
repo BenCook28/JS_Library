20b7794f166c1aa92964afd97918637b4fe53952
Ben Cook <benthomascook28@gmail.com>
2017-08-03 09:11:51 -0400

    Added 0.37 Objects Overview JS Library

---
+++ 0.37_Objects_Overview_JS_Library.js
@@ -0,0 +1,16 @@
+// Everything in JavaScript besides numbers, strings, booleans, null, and undefined
+//are objects.  Numbers, strings, and booleans have methods, but these can't change (immutable).
+//Objects are changable key: value collections.
+
+//Arrays, functions, regular expressions, and objects are objects.
+
+//An object is a container of properties.  Each property has a name and a value.
+//A preperty can be any string, including an empty string.  
+//A property value can be anything besides undefined.
+
+//Objects in JavaScript are class-free.  Instead of declaring classes, JS declares templates.
+
+//JavaScript's prototype linkage feature allows objects to inherit the properties of other objects.
+//This feature reduces initialization time and memory consumption.
+
+//Initialization the assignment of an initial value.
