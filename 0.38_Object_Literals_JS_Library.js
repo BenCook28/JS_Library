6b1e3c413843fddb309767316628d1f08ed3495b
Ben Cook <benthomascook28@gmail.com>
2017-08-04 12:46:35 -0400

    Added 0.38 Object Literals JS Library

---
+++ 0.38_Object_Literals_JS_Library.js
@@ -0,0 +1,15 @@
+//Object literals can have zero or more name: value pairs.
+//An object with zero pairs looks like this:
+
+var emptyObject = {};
+
+//An object with three pairs looks like this:
+
+var character = {
+	"FirstName": "John",
+	"LastName": "Galt",
+	"Occupation": "Inventor"
+}
+
+//The quotes around the keys are optional as long as the key isn't a reserved word.
+//We can nest objects.
