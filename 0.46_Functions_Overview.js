da220c7114fb59c79426f324c1314211a2cb9cfd
Ben Cook <benthomascook28@gmail.com>
2017-08-12 15:42:31 -0400

    Added 0.46 Functions Overview JS Library

---
+++ 0.46_Functions_Overview.js
@@ -0,0 +1,17 @@
+//A function in JavaScript encloses a set of statements and 
+//can be used over and over again with different arguments.
+//Arguments are what we pass into the function.
+
+//Functions are the modular unit of JavaScript.
+//In addition to code reuse, we can use functions to:
+
+//*Handle information: passing around data and 
+//doing different things depending on what we pass in.
+
+//*Composition: Function composition is the process 
+//of combining two or more functions to produce a new function.
+
+//*Specifying the behavior of objects.
+
+//In general, programming often involves building functions
+//and data structures to meet requirements
