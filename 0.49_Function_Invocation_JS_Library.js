859a561440f0b54595aaac8908ec4624cb4496ee
Ben Cook <benthomascook28@gmail.com>
2017-08-15 09:00:54 -0400

    Added 0.49 Function Invocation JS Library

---
+++ 0.49_Function_Invocation_JS_Library.js
@@ -0,0 +1,15 @@
+//If one function is already running, invoking a function stops
+//the current function and passes control and parameters to the 
+//new function.  
+
+//In addition to declared parameters, functions have this and arguments 
+//properties.  The invocation pattern determines the this parameter.
+//There are four invocation patterns: the method, function, constructor,
+//and apply.
+
+//The invocation operator is () after any expression that makes a function value.
+//Each expression produces one argument object value.  
+
+//If there are more arguments then the function uses, the function ignores the
+//exra values.  If there are fewer arguments, JavaScript passes in the value
+//of undefined for those values.  Any type of value can be passed to any parameter.
