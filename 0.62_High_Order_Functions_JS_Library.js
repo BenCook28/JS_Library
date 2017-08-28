f8be54d5057924468bc229c117073f33710b94bb
Ben Cook <benthomascook28@gmail.com>
2017-08-28 09:23:52 -0400

    Added 0.62 Functions High Order JS Library

---
+++ 0.62_High_Order_Functions_JS_Library.js
@@ -0,0 +1,17 @@
+// Functions are first-class objects in JavaScript. 
+// Returning a function from another function is perfectly legal. 
+
+// A higher-order function is a function that can take another function 
+// as an argument, or that returns a function as a result.
+
+// Javascript treats functions like objects.
+// We can assign functions to variables in JavaScript.
+
+// A callback is a function that gets executed at the end of an operation, 
+// once all of the other operations have been completed.
+// Usually this callback function is passed in as the last argument in the function. 
+// Frequently, it’s defined inline as an anonymous function.
+
+// JavaScript is single-threaded, meaning that only one operation happens at a time.
+Understand the techniques that closures and higher order 
+functions allow us to use.
