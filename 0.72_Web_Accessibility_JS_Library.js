aee49a8d0485c7612068f39782f345a28373b133
Ben Cook <benthomascook28@gmail.com>
2017-09-07 16:52:10 -0400

    Added 0.72 Web Accessibility JS Library

---
+++ 0.72_Web_Accessibility_JS_Library.js
@@ -0,0 +1,19 @@
+// Ways to comply:
+
+//* Give images and links meaningful names.
+
+//*  Underline or otherwise DIFFERENTIATE links, for example with capitalization, 
+// so color blind people can see them.
+
+//* Similarly, use tools to check your color scheme to enure that people with 
+//various types of color blindness can see your colors.
+
+//* Make clickable areas large for people who struggle to controll a mouse with precision.
+
+//* Make it possible for users to navigate with the keyboard alone or with a switch access device alone.
+
+//* Add closed captioning or sign language versions.
+
+//* Minimize or avlid flashing effects.
+
+//* Write in plain languageand use illustrations, diagrams, and animations.
