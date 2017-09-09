a70a16493e8dcb1a9f3b8e097a56f2819408c11a
Ben Cook <benthomascook28@gmail.com>
2017-09-08 23:50:35 -0400

    Added 0.73 NPM Prune JS Library.js

---
+++ 0.73_NPM_Prune_JS_Library.js
@@ -0,0 +1,7 @@
+// NPM prune removes all packages not listed in the 
+// package.json dependencies list and which we don't 
+// install with npm install --save.
+
+// To run this magical command, type npm prune
+// when you've navigated to your project's folder
+// in a terminal.  
