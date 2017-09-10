1b252f28b048cb0055abf366094bf4e616058fc3
Ben Cook <benthomascook28@gmail.com>
2017-09-10 12:31:11 -0400

    Added 0.74 ES2015 const JS Library

---
+++ 0.74_ES2015_const_JS_Library.js
@@ -0,0 +1,8 @@
+// By themselves, "magic numbers" don't mean much.
+// For example, the 28 in (if if arrayName.length > 28)
+// is a magic number.  Magic numbers aren't clear to other developers.
+
+// const creates read-only named constants like const favoriteNum = 28.
+// const variables must be assigned an initial value.
+
+// JS doesn't hoist const variables.
