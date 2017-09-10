0adbd83dfd7a2210b36f2a874951dd476df72ec6
Ben Cook <benthomascook28@gmail.com>
2017-09-09 20:36:26 -0400

    Added 0.73 ES2015 let JS Library

---
+++ 0.73_ES2015_let_JS_Library.js
@@ -0,0 +1,15 @@
+// Previously know as ES6
+
+// JS hoists var declarations to the top of the available scope.
+// JS scopes let variables to the nearest block and doesn't hoist them.
+// A block is any code section within curly braces.
+
+// Using (let i in arrayName) prevents hoisting in for loops with callabcks 
+// and gets us the output we want.
+// A function as an argument is a callback.
+// ES2015 creates a new variable on each iteration.
+
+// Variables declared with let can be reassigned but can't be redeclared
+// within the same scope.
+
+// Const variables are read only, while let variables are not.
