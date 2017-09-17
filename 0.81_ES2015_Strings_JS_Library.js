5b02cf22eb999159ffb355121c03f2a70ea6df91
Ben Cook <benthomascook28@gmail.com>
2017-09-17 14:20:24 -0400

    Added 0.81 ES2015 Strings JS Library

---
+++ 0.81_ES2015_Strings_JS_Library.js
@@ -0,0 +1,18 @@
+// Template Strings
+// Instead of first + " " + last;,
+// we do `${first} ${last}`;
+
+// We can also write multi-line
+// strings with template strings.
+// for example:
+
+let message = `Hi ${user},
+
+this is 
+
+a 
+
+message.
+
+Sincerely,
+${admin.fullname}`;
