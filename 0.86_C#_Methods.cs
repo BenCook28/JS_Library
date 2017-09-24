f1d52974e17aee752114911eba0c02ab025d3a91
Ben Cook <benthomascook28@gmail.com>
2017-09-24 17:15:03 -0400

    Added 0.86 C# Methods JS Library

---
+++ 0.86_C#_Methods.cs
@@ -0,0 +1,17 @@
+// a console application is a program designed to run
+// on a text-only interface like a terminal.
+
+// C# console applications have a .csproj file and a
+// Program.cs file.  Program.cs is the entry point
+// for the application.
+
+// Main in static void Main(string[] args)
+// is a method.  Methods let us break our code
+// into resuable functional blocks.
+
+// Methods have names and parameters.
+// Names in C# tend to use PascalCase, whith
+// the first letter of each word capitalized.
+
+// We must prefix parameters by their data type.
+// Parameter names typically use camelCase.
