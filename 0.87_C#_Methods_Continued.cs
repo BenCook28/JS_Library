29f1b66fc0813400f88f467c6302e53a53f4cead
Ben Cook <benthomascook28@gmail.com>
2017-09-25 09:10:39 -0400

    Added more info about C# methods

---
+++ 0.87_C#_Methods_Continued.cs
@@ -0,0 +1,14 @@
+
+// Methods require a return type and return statement.
+
+// In the method below:
+string SayHi(string yourName){
+    return "Hi " + yourName;
+}
+// the return type is string and is right before SayHi.
+// The return statement is between the {}s.
+
+// void indicates no return value.
+
+// In C# methods, “static” means “relating to the 
+// type itself, rather than an instance of the type”.
