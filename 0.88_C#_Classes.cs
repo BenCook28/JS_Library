f766af7f4c92ce85d29dd48bb7cceafae0ddef74
Ben Cook <benthomascook28@gmail.com>
2017-09-26 10:33:45 -0400

    Added 0.88 C# Classes JS Library

---
+++ 0.88_C#_Classes.cs
@@ -0,0 +1,14 @@
+// A class defines the structure of an object.
+
+// A class looks like the below, as might be found
+// in Band.cs:
+class Band
+{
+    string Name;
+    int Musicians;
+    // Methods can go here too.
+}
+
+// an object looks like the below:
+Name: "Journey"
+Musicians: 4
