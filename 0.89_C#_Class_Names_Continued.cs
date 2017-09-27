b5a5ec9008b2cc2781aaa33de2c78a4b7e555e69
Ben Cook <benthomascook28@gmail.com>
2017-09-27 12:36:53 -0400

    Added 0.89 C# Class Names Continued JS Library

---
+++ 0.89_C#_Class_Names_Continued.cs
@@ -0,0 +1,16 @@
+// Namespaces organize classes further
+// allowing the reuse of class names.
+
+// We can access Namespaces using
+//Namespace.Class.Method
+
+// For example, to get Console, we type
+System.Console.WriteLine()
+
+// A using directive provides a shorter way
+// to get the same results.
+// For example:
+using System;
+
+// lets us just type
+Console.WriteLine();
