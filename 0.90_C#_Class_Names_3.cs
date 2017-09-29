86d7db2c5929254f846b38031aa638bab787f643
Ben Cook <benthomascook28@gmail.com>
2017-09-29 09:55:17 -0400

    Added 0.90 C# Class Names JS Library

---
+++ 0.90_C#_Class_Names_3.cs
@@ -0,0 +1,7 @@
+using Band;
+public class Band // makes the code accessible anywhere.
+// private makes code available in the same class only.
+// We can make variables and functions public too.
+
+Band band = new Band();
+// creates a new instance of the Band class.
