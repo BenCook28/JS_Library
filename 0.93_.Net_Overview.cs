eecd88a44199f260181f291ec26fa96ab1ae3001
Ben Cook <benthomascook28@gmail.com>
2017-10-01 18:27:07 -0400

    Added 0.93 .Net Overview JS Library

---
+++ 0.93_.Net_Overview.cs
@@ -0,0 +1,15 @@
+// views are in cshtml format.
+// Using the same name across files
+// synces MVC.
+
+// @model String says our view expects
+// a single string of data.
+
+// To access model data in our view,
+// we use @Model.
+
+// IActionResult gets returned in
+// action methods, which provide 
+// responses usable to browsers.
+
+// home/index is typically the default route.
