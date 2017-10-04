42a042aff8ad9042fa354604824ad9d53be39513
Ben Cook <benthomascook28@gmail.com>
2017-10-03 21:51:39 -0400

    Added 0.95 Xamarin tips JS Library

---
+++ 0.95_Xamarin_Tips.cs
@@ -0,0 +1,10 @@
+// dp, as in 100dp occupies the same area on
+// a screen regardless of screen density.
+
+// android:layout_width="match_parent"
+//  makes something the same width as a 
+//  parent view.
+
+// android:layout_height="wrap_content"
+// makes the height just big enough to fit 
+// the content.  160dpi is the base dpi.
