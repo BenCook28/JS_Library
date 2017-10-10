7ea5dc79306e8c15230053a42f7c225896543ed8
Ben Cook <benthomascook28@gmail.com>
2017-10-10 08:34:20 -0400

    Added 1.02 ES2015 WeakMaps JS Library

---
+++ 1.02_ES2015_WeakMaps.js
@@ -0,0 +1,8 @@
+// Only objects can be passed in as keys in 
+// WeakMaps.  We can use .get(key)
+// .has(key), and .delete(key) methods in weak maps.
+
+// WeakMaps are not iterable.
+// WeakMaps are better for memory.
+// The objects referenced can be garbage collected
+// when no longer used.
