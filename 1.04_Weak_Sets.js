834e890c9a0671e899dc3f80561ecd39ec014833
Ben Cook <benthomascook28@gmail.com>
2017-10-12 08:33:41 -0400

    Added 1.04 ES2015 Weak Sets JS Library

---
+++ 1.04_Weak_Sets.js
@@ -0,0 +1,11 @@
+// WeakSets also exist, and only accept objects,
+// The .has(value) property returns a boolean.
+
+// The .delete(value) property deletes, then 
+// returns true if the value existed.
+
+// We can't iterate over WeakSets, and we
+// can't read values from WeakSets.
+
+// We don't mutate objects when we use
+// WeakSets.
