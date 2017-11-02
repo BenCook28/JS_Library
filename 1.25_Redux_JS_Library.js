5ee02203a480825b2c809d7225b5a4495fbbf1f1
Ben Cook <benthomascook28@gmail.com>
2017-11-02 18:07:23 -0400

    Added 1.25 Redux JS Library

---
+++ 1.25_Redux_JS_Library.js
@@ -0,0 +1,20 @@
+// Redux is a React library.
+
+// Redux solves the problem of mutable
+// state, which means any part of the
+// application can change a state.  With 
+// Redux, only actions change the state
+// of the immutable store.
+
+// It also reduces code duplication through 
+// action creators.
+
+// It also makes code easier to mantain.
+// Subscriptions update the state whenever
+// something changes.  Reducers allow 
+// state manipulation to happen in one place.
+
+// The current version of 
+// Redux works with React Native.
+
+// Elm inspired Redux.
