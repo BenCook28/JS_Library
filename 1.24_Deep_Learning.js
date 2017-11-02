207535e81f827f4970b384241f570bdd01948310
Ben Cook <benthomascook28@gmail.com>
2017-11-01 23:03:43 -0400

    Added 1.24 Machine Learning JS Library

---
+++ 1.24_Deep_Learning.js
@@ -0,0 +1,14 @@
+// Deep learning is a subset of machine 
+// learning which is a subset of artificial
+// intelligence.
+
+// Artificial intelligence: Computers doing 
+// things which humans typically do.
+
+// Machine learning: when even the 
+// developers don't know how the computers
+// do what they do.
+
+// Deep learning: When computers learn
+// from data without labels and whithout
+// instruction from humans.
