d8c474617a792788799321514899827865ea4a05
Ben Cook <benthomascook28@gmail.com>
2017-11-03 23:41:18 -0400

    Added 1.26_Things_I_Learned_Recently_1.js

---
+++ 1.26_Things_I_Learned_Recently1.js
@@ -0,0 +1,15 @@
+// If I run react-native run-ios,
+// then I don't have to run nodemon 
+// index.js, gulp, or any other
+// method I've used to run apps locally.
+
+// I need to install ESLint on every
+// project in order to use it, even
+// though I installed part of it globally.
+
+// Mixins aren't used anymore in React Native
+// for calling functions at a later time.
+
+// On a MacBook Pro CMD Left arrow works the
+// same as the home key and CMD right arrow
+// works the same as the end key.
