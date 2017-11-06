0df30bc723c050ab3f5eafc9b966002096708760
Ben Cook <benthomascook28@gmail.com>
2017-11-05 19:43:32 -0500

    Added 1.27 Rendering Components React Native JS Library

---
+++ 1.27_Rendering_Components_React_Native_JS_Library.js
@@ -0,0 +1,15 @@
+// A helper method is method that
+// helps another method perform its'
+// task.
+
+// The .map() function is an ES2015 
+// function for iterating over arrays.
+
+// .map() takes a fat arrow function as 
+// a parameter.  It looks like this:
+
+// renderAlbums() {
+// 	this.state.albums.map(album => <Text>{album.title}</Text>)
+// }
+
+// album is one item in the array.
