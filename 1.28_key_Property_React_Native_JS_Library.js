db467bcee42f7201a6327a3a87fbc22e397efb46
Ben Cook <benthomascook28@gmail.com>
2017-11-06 08:17:14 -0500

    Added 1.28 Key Property React Native JS Library

---
+++ 1.28_key_Property_React_Native_JS_Library.js
@@ -0,0 +1,8 @@
+// Key properties when used 
+// with the map() function
+// tell React which array item
+// to render.  The array index
+// might not be consistent.
+
+// It looks like this:
+// <Text key={album.title}>
