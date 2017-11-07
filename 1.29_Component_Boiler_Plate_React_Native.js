ae7026d2ef2ab40be0c80826ebca991cc4837f42
Ben Cook <benthomascook28@gmail.com>
2017-11-07 06:44:54 -0500

    Added 1.29 Component Boilerplate React Native JS Library

---
+++ 1.29_Component_Boiler_Plate_React_Native.js
@@ -0,0 +1,23 @@
+// We use functional components
+// for display purposes.  It looks 
+// like this:
+
+const AlbumDetail = () => {
+
+}
+
+// We use class based components 
+// when data will change and 
+// we need to access state.
+// It looks like this:
+
+class AlbumList extends Component {
+	state = '';
+
+	render (){
+
+		return <View></View>
+	}
+}
+
+export default AlbumList;
