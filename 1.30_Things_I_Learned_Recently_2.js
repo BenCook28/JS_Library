6427e38f188d189d64f6fe67dda0c81730410282
Ben Cook <benthomascook28@gmail.com>
2017-11-08 08:53:40 -0500

    Added 1.30 Things I Learned Recently 2 JS Library

---
+++ 1.30_Things_I_Learned_Recently_2.js
@@ -0,0 +1,12 @@
+// require http only works on the
+// server side.
+
+//On the server side, we need to 
+// use the following syntax when
+// we import axios:
+
+var axios = require ('axios');
+
+// flow is like eslint but works 
+// more deeply and affects your 
+// code.
