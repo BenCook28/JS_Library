954aa388ebda87a7b2e048d8a5b0ad553b35cb8c
Ben Cook <benthomascook28@gmail.com>
2017-11-09 21:53:42 -0500

    Added 1.31 Things I Learned Recently 3 JS Library

---
+++ 1.31_Things_I_Learned_Recently_3.js
@@ -0,0 +1,16 @@
+// I made a mistake in my entry from 
+// yesterday, with Axios posts the
+// correct order is link, data, headers.
+
+// Pinching with four fingers on a 
+// Mac shows all open apps.
+
+// We access the value associated
+// with an object key with Object[key].
+
+// The Object && Object syntax works in
+// a similar way as the terinary operator.
+// it says, "does something exist or is it true?
+
+// We can create package.json files within a 
+// subfolder so shorten the paths to files.
