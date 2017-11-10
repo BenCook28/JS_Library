b0e1a25a20ca6de71c6da702f5fd3185896135fd
Ben Cook <benthomascook28@gmail.com>
2017-11-10 18:30:24 -0500

    Added 1.32 Things I Learned Recently 4 JS Library

---
+++ 1.32_Things_I_Learned_Recently_4.js
@@ -0,0 +1,14 @@
+// Whenever I don't know what's wrong
+// with a React Native application,
+// I can kill the packager and restart it.
+
+// git doesn't handle capitalization changes
+// well. The convention is to use lowercase
+// with the components folder.
+
+// We can use Viro Media for VR and AR with
+// React Native.
+
+// We don't need to pass props through a 
+// function because React Native already
+// has access to them.
