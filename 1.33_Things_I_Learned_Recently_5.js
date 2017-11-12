0964a714ec9348632c263eaf1927e2138d67e6ce
Ben Cook <benthomascook28@gmail.com>
2017-11-11 20:27:40 -0500

    Added 1.33 Things I Learned Recently 5 JS Library

---
+++ 1.33_Things_I_Learned_Recently_5.js
@@ -0,0 +1,18 @@
+// Swiping on the track pad with
+// three fingers on a Mac works 
+// the same as the Control + Arrow
+// shortcut.
+
+// The ES2015 spread operator ...
+// sepearates the items in an
+// array so they can more
+// easily become variables.
+
+// Oauth gives users both a 
+// token for authentication and
+// a refresh token which expires in
+// 90 days.  
+
+// In ES6, instead of the syntax
+// router: router, we would just
+// enter router .
