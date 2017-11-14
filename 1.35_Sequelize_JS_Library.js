907556b739017cc320a3eec8bb183edb51681d16
Ben Cook <benthomascook28@gmail.com>
2017-11-13 21:34:35 -0500

    Added 1.35 Sequelize JS Library

---
+++ 1.35_Sequelize_JS_Library.js
@@ -0,0 +1,14 @@
+// With Sequelize, we can 
+// use the same code
+// related to getting data into
+// SQL databases and get the code 
+// into Postgres, MySQL, Microsoft
+// SQL, Oracle SQL, or any other kind
+
+// We specify the dialect of SQL in
+// the models folder, and Sequelize
+// does the rest.  
+
+// Thanks to Sequelize, users don't
+// have to re-make database schemas
+// when they clone code.
