2297117889300093c26f2a9d4e00ed6d3d9140bf
Ben Cook <benthomascook28@gmail.com>
2017-11-15 19:24:29 -0500

    Added 1.37 Flux and Passing Props JS Library

---
+++ 1.37_Things_I_Learned_Recently_7.js
@@ -0,0 +1,17 @@
+// In addition to passing components,
+// We can pass data from one component 
+// to another using props.
+
+// Flux makes it easier to see where
+// data is coming from in larger 
+// applications when data would
+// otherwise pass into deeply nested
+// components.
+
+// Flux keeps data in a global store,
+// and a dispatcher interacts with the
+// store and changes the data.
+
+// Developers should evaluate the future
+// time savings and complex setup before
+// deciding whether Redux / Flux are worth it.
