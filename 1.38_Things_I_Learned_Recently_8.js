841eabfbc3b57b249eff24626c7a32caed18e9c7
Ben Cook <benthomascook28@gmail.com>
2017-11-16 20:13:01 -0500

    Added 1.38 Things I Learned Recently 8

---
+++ 1.38_Things_I_Learned_Recently_8.js
@@ -0,0 +1,18 @@
+// We don't put const or let
+// before variables in React
+// classes, but we do
+// put const and let in front
+// of variables within functions.
+
+// ES6 concatination looks like
+// this:  `text${JS}more_text`
+
+// For form validation, !username
+// covers both undefined and null.
+
+// We can use React Navigator to go 
+// back more than one view.
+
+// We can use sequelize command line
+// tools to create new tables and 
+// migrate them to the database.
