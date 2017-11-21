51435bc0782bf03e1e66c47aa255e3d5897a7005
Ben Cook <benthomascook28@gmail.com>
2017-11-20 20:25:29 -0500

    Added 1.42 Things I Learned Recently 9

---
+++ 1.42_Things_I_Learned_Recently_9.js
@@ -0,0 +1,14 @@
+// After we connect a component to Redux,
+// we pass in a desired variable:
+// this.props.//variable name.
+
+// With MobX, data automatically updates
+// but it can be harder to trace
+// the flow of data than with Redux,
+// but Redux can be more verbose.
+
+// We can pass styles in React
+// as props, which we wrap in
+// curley braces.  React styles
+// use camelCase because they're
+// JavaScript.
