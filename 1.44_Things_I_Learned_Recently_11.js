8b545ff5a15c59c27988b099814bef93ba0e3cf0
Ben Cook <benthomascook28@gmail.com>
2017-11-22 23:24:53 -0500

    Added 1.44 Things I Learned Recently 11

---
+++ 1.44_Things_I_Learned_Recently_11.js
@@ -0,0 +1,15 @@
+// If we install a new node modules
+// and Node or React Native doesn't
+// recognize it, we can delete
+// node modules and then run
+// npm install.
+
+// Stateless components are for
+// presenting data only,
+// and require less code
+// writing and improve
+// performance.
+
+// Because stateless components
+// are functions and not
+// classes, they have no this.
