26efde275b80daeae0fb3e0b5f6fdf02f27937d7
Ben Cook <benthomascook28@gmail.com>
2017-11-23 11:33:17 -0500

    Added 1.45 Pros and Cons of Inline Styles with React

---
+++ 1.45_Pros_and_Cons_of_Inline_Styles_React.js
@@ -0,0 +1,16 @@
+// Pros:
+// Inline styles allow for code 
+// reuse becausse we define 
+// code in the component's 
+// stylesheet, then pass
+// the component.  They 
+// are also easy to use.
+
+// Cons:
+// But inline styles take 
+// longer to render, though
+// the time difference mainly
+// shows on giant apps. And
+// they don't offer hover
+// and certain other 
+//  functionality.
