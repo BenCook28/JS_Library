9840ac3d22b5dc965be164dee125bb8218ad623a
Ben Cook <benthomascook28@gmail.com>
2017-11-24 11:41:25 -0500

    1.46 Things I Learned Recently 12

---
+++ 1.46_Things_I_Learned_Recently_12.js
@@ -0,0 +1,10 @@
+// nodemon doesn't recongnize ES6
+// features as of Nov. 23, 2017.
+// Instead I can run an Express
+// server useing babel-node 
+// in nodemon's place.
+
+// Also, many features which
+// used to come with Express
+// now have to be included 
+// seperately.
