7b3ebb6c14f19a6121b37d86110ce6bd78e06607
Ben Cook <benthomascook28@gmail.com>
2017-11-25 18:30:53 -0500

    1.47 Things I Learned Recently 13

---
+++ 1.47_Things_I_Learned_Recently_13.js
@@ -0,0 +1,16 @@
+// We can define the database url 
+// in the settings tab of the
+// Heroku interface, get the 
+// url through the Heroku CLI,
+// and create a variable in the
+// Heroku interface which determines
+// whether there's a database in
+// Heroku.  
+
+// We can use grep to 
+// find text in the terminal,
+// after we enter the word history
+// in the terminal, then run 
+//!WhateverNumberCorresponds-
+// withTheCommandLine command
+// we want to run again.
