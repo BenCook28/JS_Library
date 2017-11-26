026aded29835f6199aeab8338d90a2dffb93a03f
Ben Cook <benthomascook28@gmail.com>
2017-11-26 12:12:08 -0500

    Added 1.48 Controlled and Uncontrolled React Components

---
+++ 1.48_Controlled_and_Uncontrolled_React.js
@@ -0,0 +1,9 @@
+// The browser controls "uncontrolled"
+// components, as with
+// <input type="text" />
+// because React doesn't control
+// the process.
+
+// But React controlled components
+// may involve state, props, and 
+// a handle submit functions.
