3df64f97e1a3fc13a7c60f99754cf41390affb0e
Ben Cook <benthomascook28@gmail.com>
2017-11-28 21:34:28 -0500

    Added 1.50 Things I Learned Recently 14

---
+++ 1.50_Things_I_Learned_Recently_14.js
@@ -0,0 +1,16 @@
+// Axios doesn't work with 
+// Android, but works with
+// iOS.  We need to put
+// gradle variables in
+// strings.  We archive
+// Xcode builds through the
+// bar that appears when
+// we hover near the top
+// of the screen.  With
+// .jsx files, we get
+// formatting suggestions
+// which can reduce errors.
+// Instead of Axios, we
+// can use fetch with 
+// Android code to access
+// APIs.
