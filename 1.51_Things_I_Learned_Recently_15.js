377b7a1442095a5fbfbf784c7c5a23056d72fa39
Ben Cook <benthomascook28@gmail.com>
2017-11-29 22:38:40 -0500

    1.51 Things I Learned Recently 15

---
+++ 1.51_Things_I_Learned_Recently_15.js
@@ -0,0 +1,9 @@
+// We run the Anroid emulator
+// from the tools folder.
+// Because the phone 
+// simulator has a
+// different IP address,
+// we set an IP address where
+// we usually declare localhost
+// in the React Native code
+// to fix network errors.
