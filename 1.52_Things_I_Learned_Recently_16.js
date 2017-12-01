49cfd03738916f48244bd484377b59a5e2244937
Ben Cook <benthomascook28@gmail.com>
2017-11-30 21:45:32 -0500

    Things I Learned Recently 16

---
+++ 1.52_Things_I_Learned_Recently_16.js
@@ -0,0 +1,15 @@
+// With the CSS Modules approach to
+// styling with react, we first
+// import a stylesheet module
+// at the top like so:
+
+	
+// import styles from "./style.css";
+
+// Then follow the steps here:
+// https://github.com/css-modules/css-modules
+
+// for these reasons:
+// No more conflicts.
+// Explicit dependencies.
+// No global scope.
