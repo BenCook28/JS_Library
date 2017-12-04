4b882a34efb0f5559fe808ec2962f1670667dfbb
Ben Cook <benthomascook28@gmail.com>
2017-12-03 20:51:50 -0500

    Added 1.55 CSS Modules

---
+++ 1.55_CSS_Modules.js
@@ -0,0 +1,16 @@
+// A CSS Module is a CSS file in which
+//  all class names and animation names 
+//  are scoped locally by default. 
+
+// With CSS Modules, we don't risk
+// reusing class names or overwriting
+// styles accidentally.
+
+// We can also import only the styles
+// we actually need.
+
+// There's also no global scope with 
+// CSS modules.
+
+// CSS modules are also familiar to 
+// developers who know CSS.
