a1d89cb6c09bafde3df9846627406d98cb353b28
Ben Cook <benthomascook28@gmail.com>
2017-12-04 21:42:28 -0500

    Added 1.56 Resolving Android Simulator React Native Network Error

---
+++ 1.56_Resolving_Netork_Error_Android_Simulator.js
@@ -0,0 +1,11 @@
+// Android simulators have
+// their own IP address.
+// so to point to our
+// computer's IP address,
+// we type ifconfig in the 
+// command line, find our
+// IP address, then put the 
+// localhost (for example, 3000)
+// at the end as shown below:
+
+// fetch('http://192.168.0.2:3000/'
