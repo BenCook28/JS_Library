fe51f1f34e78e4759d0b4277ddaa27fc0fbaa1ae
Ben Cook <benthomascook28@gmail.com>
2017-12-05 18:16:59 -0500

    setting upstream

---
+++ 1.57_Accessing_Redux_Reducers.js
@@ -0,0 +1,11 @@
+// We don't need to import Reducers.
+// In redux connect statements, the
+// mapStateToProps function goes first,
+// followed by an optional second
+// argument which covers what we'll
+// pass into the store if anything,
+// and the third argument is the name
+// of the module we're exporting.
+
+// Also, this sets upsteam:
+// git push -u origin my_branch
