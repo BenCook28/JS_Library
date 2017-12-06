41f6e6552fbc64f4cc8da60c6e0f749bac8f93f8
Ben Cook <benthomascook28@gmail.com>
2017-12-06 18:40:41 -0500

    Added 1.58 Substring

---
+++ 1.58_substring.js
@@ -0,0 +1,6 @@
+// substring returns a shorter string,
+// for example, we might have a long
+// date with a timestamp and time zone
+// stored in the variable longDate.
+// If we only want a date like 2017-12-06,
+// we could run longDate.substring(0, 10);
