e7286a2c8747170d45c64fce4a49f5ecb0464c9c
Ben Cook <benthomascook28@gmail.com>
2017-12-07 19:39:07 -0500

    Added 1.59 Things I Learned Recently 19

---
+++ 1.59_Things_I_Learned_Recently_19.js
@@ -0,0 +1,10 @@
+// To render multiple JSX elements
+// we wrap the elements in the same
+// view tag and give this view tag
+// the flex: 1 style.
+
+// Finicity's Add All Accounts
+// API route requires demo username
+// and passwords, and we have 
+// to use double quotes instead of
+// single quotes.
