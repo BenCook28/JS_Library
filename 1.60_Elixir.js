e609d88787f74c12f56e9aceb798c8947a963c7e
Ben Cook <benthomascook28@gmail.com>
2017-12-08 21:23:21 -0500

    Added 1.60 Elixir

---
+++ 1.60_Elixir.js
@@ -0,0 +1,17 @@
+// Elixir leverages the Erlang
+// VM, which the phone company
+// Ericsson built to ensure 
+// high uptime.  Most
+// improvements in application
+// performance will come from
+// adding more cores and virtual
+// cores unless we discover new 
+// materials.  Cores take data
+// inputs and produce outputs,
+// so they're like mini computers
+// Elixir thrives on multiple cores.
+// Elixir is a functional programming 
+// language, which rocks because many 
+// bugs come from mutating the state
+// and functional langauges don't mutate
+// the state.
