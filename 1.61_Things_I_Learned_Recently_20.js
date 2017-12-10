699047612b3edcff8b27b13f6b87f356e2fe2875
Ben Cook <benthomascook28@gmail.com>
2017-12-09 22:56:46 -0500

    Added 1.61 Things I Learned Recently 20

---
+++ 1.61_Things_I_Learned_Recently_20.js
@@ -0,0 +1,14 @@
+// To open Spectacle to work with
+// two screens simultaneously on 
+// Macs, we press command + space
+// from any app, then press 
+// command + option + arrow key
+ 
+ // We can assign values to
+ // obnects in the same way
+ // we can assign values to 
+ // array keys.
+
+ // In JavaScript, months begin 
+ // at zero, so a month of 11 means
+ // December.
