7b77e83919bb53a71af44b6e1a6d2367cbeffdb9
Ben Cook <benthomascook28@gmail.com>
2017-12-10 20:05:12 -0500

    Added 1.62 React Uncontrolled Components

---
+++ 1.62_React_Uncontrolled_Components.js
@@ -0,0 +1,9 @@
+// When we use uncontrolled components,
+// we use ref properties to directly access
+// the DOM.  
+
+// The ref prop is a function with the DOM 
+// element as its argument.
+
+// React recommends using controlled components
+// so we don't interact directly with the DOM.
