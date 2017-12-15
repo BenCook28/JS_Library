b425f957bd0987ca6f1a570272e508c8a630ddf4
Ben Cook <benthomascook28@gmail.com>
2017-12-14 20:55:27 -0500

    Added 1.64 Things I Learned Recently 21

---
+++ 1.64_Things_I_Learned_Recently_21.js
@@ -0,0 +1,16 @@
+// Instead of going through 
+// the Android SDK process
+// I meantioned yesterday,
+// we could just adjust the
+// version number and build
+// number in the React Native
+// folder > Android > build.gradle
+// or something like that.
+// then we could run the terminal
+// command in the React Native docs.
+
+// If wae get errors in Heroku logs
+// related to process.env, we may
+// need to add the production 
+// variables to the settings
+// tab of the Heroku user interface
