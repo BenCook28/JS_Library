0225c299cc2504a15612970f36843347936041bb
Ben Cook <benthomascook28@gmail.com>
2017-12-15 23:45:46 -0500

    Added 1.65 map

---
+++ 1.65_map.js
@@ -0,0 +1,16 @@
+// maps in ES6 take an array
+// and return a new array 
+// as an output, thus not
+// mutating the state.
+
+// In the render function, 
+// we can iterate over the
+// new array with something
+// like 
+
+// this.props.transactions.map(renderTransaction).
+
+// after putting key={i} in the 
+// renderTransactions function.
+// renderTransactions could have
+// any name.
