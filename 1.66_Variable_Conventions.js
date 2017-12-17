70fcd90c60f05f7a5a23542d1b65a4113ac417b7
Ben Cook <benthomascook28@gmail.com>
2017-12-16 19:18:52 -0500

    Added 1.66 Variable Conventions

---
+++ 1.66_Variable_Conventions.js
@@ -0,0 +1,16 @@
+// When a database column name
+// contains multiple words,
+// we separate them with 
+// underscores like_this.
+
+// JavaScript variables 
+// containing multiple
+// words use camelCase.
+
+// We need to install
+// the sequelize command
+// line interface in
+// the dependencies
+// in order to migrate
+// sequelize tables to
+// Heroku.
