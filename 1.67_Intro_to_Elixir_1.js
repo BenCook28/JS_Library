e36c25fe608ea489e4f1469b6db891dae9f9a2c9
Ben Cook <benthomascook28@gmail.com>
2017-12-17 21:35:08 -0500

    Added 1.67 Intro to Elixir 1

---
+++ 1.67_Intro_to_Elixir_1.js
@@ -0,0 +1,6 @@
+// It is possible to have a JavaScript
+// front end and Elixir backend.
+// A 20 million line program 
+// opperated for four years
+// with no downtime because
+// of the Erlang VM.
