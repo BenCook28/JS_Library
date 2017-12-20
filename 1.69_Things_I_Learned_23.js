7d9a1dc7a71668bb5506050d40269f2dd34165ce
Ben Cook <benthomascook28@gmail.com>
2017-12-20 18:44:27 -0500

    Added 1.69 Things I Learned Recently 23

---
+++ 1.69_Things_I_Learned_23.js
@@ -0,0 +1,14 @@
+// Through return statements
+// in promise chains, we
+// can pass variables from
+// the functions earlier in
+// the promise chain to the
+// functions later in the
+// promise chain.
+
+// Hackhands is an alternative
+// to Codementor.  We can
+// pass multiple actions
+// to mapDispatchToProps
+// through the spead 
+// operator.
