9548aba46e5dd149c132a3249b26f03fe510a43b
Benjamin Cook <benthomascook28@gmail.com>
2017-12-25 19:53:06 -0500

    Added 1.71 High Order Components

---
+++ 1.71_High_Order_Components.js
@@ -0,0 +1,11 @@
+// High order components are 
+// functions that take a 
+// component as input and return
+// a new component without mutating
+// te original component.
+
+// High order components are not
+// part of React, but are a pattern
+// we can use with React.  Connect
+// qualifies as a high order component
+// (HOC)
