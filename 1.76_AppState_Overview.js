e9ca96b63dab6992b15c3a8470bcece76397a7b3
Benjamin Cook <benthomascook28@gmail.com>
2017-12-30 21:30:15 -0500

    Added 1.76 AppState Overview

---
+++ 1.76_AppState_Overview.js
@@ -0,0 +1,14 @@
+// AppState in React Native identifies
+// whether the app is actively running
+// so we can force logouts for security.
+
+// active means the app is running the 
+// the foreground.
+
+// background means the app is running in
+// the background because the user is
+// on the home scren or another app.
+
+// inactive results from periods of 
+// inactiveity or transitions from
+// one state to another.
