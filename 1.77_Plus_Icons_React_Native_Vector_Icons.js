33db7d1fc4607e946450cdf2588a3046a15c33cd
Benjamin Cook <benthomascook28@gmail.com>
2017-12-31 20:24:36 -0500

    Added 1.77 Plus Icons React Native Vector Icons

---
+++ 1.77_Plus_Icons_React_Native_Vector_Icons.js
@@ -0,0 +1,13 @@
+// I wrote the following code to 
+// add a plus icon using React Native
+// Vector Icons
+
+// import Icon from 'react-native-vector-icons/Entypo';
+
+// render() {
+// 	return (
+// 	  <View>
+// 	      <Text style={styles.text}><Icon name="plus" size={30} color="#000" /></Text>
+// 	  </View>
+// 	);
+// }
