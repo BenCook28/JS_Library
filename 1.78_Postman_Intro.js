2400dadc12a9aa527b9e01da33be7d15840088ee
Benjamin Cook <benthomascook28@gmail.com>
2018-01-01 10:50:30 -0500

    Added 1.78 Postman Intro

---
+++ 1.78_Postman_Intro.js
@@ -0,0 +1,7 @@
+// Without creating a user interface,
+// I can push data into a database 
+// using Postman.  I just have to 
+// start the node server,
+// then create a route, then do 
+// a post or get request in 
+// Postman with the route's URL.
