499eb2bd7209f9feb0c164e77ce53be99ddc6310
Benjamin Cook <benthomascook28@gmail.com>
2018-01-03 21:57:49 -0500

    Added 1.80 JS Versions

---
+++ 1.80_JS_Versions.js
@@ -0,0 +1,15 @@
+// The ECMA TC39, Ecma 
+//International, Technical Committee
+// decided what features go into
+// each new release of JavaScript.
+
+// Through Github, we can see
+// proposed features
+
+// https://github.com/tc39
+
+// If a feature gets to 
+// stage three, it will
+// most likely go into
+// the next version of
+// JavaScript
