3acd5979ed1d0b8995eff94dba2e31f59190860a
Ben Cook <benthomascook28@gmail.com>
2018-01-05 23:23:01 -0500

    Added 1.82 Refreshing Web Data

---
+++ 1.82_Refreshing_Web_Data.js
@@ -0,0 +1,7 @@
+// If a website's data hasn't
+// immediately updated,
+// we can hold shift as we 
+// do the keyboard shortcut 
+// for refreshing the page.
+// cmd + r on Mac and ctrl + r
+// on PC.
