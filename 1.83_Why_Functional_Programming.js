9cfc030c9625841515a7422ab37f0017cdfddb20
Ben Cook <benthomascook28@gmail.com>
2018-01-06 15:09:44 -0500

    Added 1.83 Why Functional Programming

---
+++ 1.83_Why_Functional_Programming.js
@@ -0,0 +1,11 @@
+// Evem though functional
+// programs use more memory,
+// memory is now cheap.
+
+// Functinal programs
+// are simplier, easier
+// to maintain, and
+// are much less likely
+// to have unintended 
+// side effects and cause
+// bugs.
