d8942220943368a4fee365ed7e1d624846d108cd
Ben Cook <benthomascook28@gmail.com>
2018-01-07 15:49:26 -0500

    Added 1.84 Mob Programming

---
+++ 1.84_Mob_Programming.js
@@ -0,0 +1,8 @@
+// In mob programming, one person
+// programs while talking aloud
+// as other people think aloud
+// and suggest possible ways
+// to code.  Rob Martin describes
+// Mob programming in his Lamda 
+// Days talk on teaching functional
+// programming to new developers.
