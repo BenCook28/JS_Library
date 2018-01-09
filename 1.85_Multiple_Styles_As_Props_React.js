4df5f94c53e1b76d115a30e665f75c988184dfa9
Ben Cook <benthomascook28@gmail.com>
2018-01-08 21:33:23 -0500

    Added 1.85 Multiple Styles as React Props

---
+++ 1.85_Multiple_Styles_As_Props_React.js
@@ -0,0 +1,6 @@
+// To add space between multiple
+// inline items we render in 
+// React, we can add {{marginnLeft:1rem, marginRight:1rem}};
+// And Create React App works 
+// great for quickly launching a 
+// skeleton to build upon.
