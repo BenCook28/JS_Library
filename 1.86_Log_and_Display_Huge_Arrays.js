5293dbad85e0501a12776a723017a1728334b353
Benjamin Cook <benthomascook28@gmail.com>
2018-01-09 21:58:42 -0500

    Added 1.86 Log and Display Huge Arrays

---
+++ 1.86_Log_and_Display_Huge_Arrays.js
@@ -0,0 +1,12 @@
+// If a console.log of a huge
+// array ends with something like
+// ... 435 more], then we can
+// JSON.stringify the array.
+// the to display everything
+// on one line, we can go to
+// a spreadsheet program,
+// find and replace the "",
+// past the resutling values
+// separated by columns, and 
+// then paste transpose to
+// get each value on its own line. 
