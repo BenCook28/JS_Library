3332d35ae6f3a3eb3bc5c52b5a84190437975214
Ben Cook <benthomascook28@gmail.com>
2018-01-10 18:23:14 -0500

    Added 1.87 Benefits of Phoenix

---
+++ 1.87_Benefits_of_Phoenix.js
@@ -0,0 +1,11 @@
+// With the Phonenix web framework
+// for Elixir, we can create
+// components, forms, and their
+// associated routes.  We can 
+// query only the database data
+// we need to query.  And the
+// framework performs quickly.
+// With containers, we can 
+// avoid some code duplication,
+// and label programs in ways 
+// non-developers understand.
