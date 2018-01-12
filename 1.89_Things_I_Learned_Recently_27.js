ef102e6cc0de33fc070d25d746218cf4e0858370
Ben Cook <benthomascook28@gmail.com>
2018-01-12 18:33:55 -0500

    Added 1.89 Things I Learned Recently 27

---
+++ 1.89_Things_I_Learned_Recently_27.js
@@ -0,0 +1,15 @@
+// Debuggers, like Node debugger,
+// reduce the number of console.logs
+// we need to write.
+
+// We don't need to splice in order 
+// to sort arrays.  If we do,
+// the array will be undefined.
+
+// The Lodash library contains
+// code for arrays, objects, 
+// and more within a functional
+// paradigm.  JavaScript also
+// contains loop types like
+// forEach which reduce the
+// code we have to write.
