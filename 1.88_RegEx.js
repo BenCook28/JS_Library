8975df8a492ba09fe8c40845bf0d02c7f4ffe73c
Benjamin Cook <benthomascook28@gmail.com>
2018-01-11 20:43:48 -0500

    Added 1.88 RegEx.js

---
+++ 1.88_RegEx.js
@@ -0,0 +1,11 @@
+// RexEx101.com offers references,
+// for what RexEx symbols look like,
+// and allows you to test your RexExs
+// on a text sample.
+
+// The RegExplained YouTube talk
+// also servers as a great resource.
+
+// Regular Expressions are often
+// similar accross programming 
+// languages, databases, terminals, etc.
