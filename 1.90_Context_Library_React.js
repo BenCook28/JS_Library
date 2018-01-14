ecd317d70b8e9f8a53633d423bd213a258de6a95
Ben Cook <benthomascook28@gmail.com>
2018-01-13 21:39:59 -0500

    Added 1.90 The React Context Library Overview

---
+++ 1.90_Context_Library_React.js
@@ -0,0 +1,11 @@
+// The Context library in React
+// makes it easy to pass props
+// deep in component trees,
+// but it can cause issues if 
+// overused.  In general, it makes
+// sense to use Context if all
+// the components will use the
+// prop, as with styling themes.
+// Otherwise, a flux pattern 
+// like Redux or MobXwould work 
+// better.
