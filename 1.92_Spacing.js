513eb4f1c7b2902f1b18ad2728da3f10443c4590
Benjamin Cook <benthomascook28@gmail.com>
2018-01-15 22:25:00 -0500

    Added 1.92 Spacing

---
+++ 1.92_Spacing.js
@@ -0,0 +1,11 @@
+// Pretify autuomtically 
+// handles styling like 
+// indentation, so it can
+// shut up your linter.
+
+// Sublime Text can
+// convert tabs to spaces
+// and set the number of spaces
+// through the bottom
+// right part of the
+// Sublime view.
