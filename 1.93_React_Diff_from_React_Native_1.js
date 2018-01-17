9b62f8a99187ead0dd45f8edae207924de1c44eb
Benjamin Cook <benthomascook28@gmail.com>
2018-01-16 19:14:20 -0500

    Added 1.93 React Diff from React Native 1

---
+++ 1.93_React_Diff_from_React_Native_1.js
@@ -0,0 +1,12 @@
+// React Native buttons use onTextChange,
+// while React buttons use onChange.
+
+// React functions need to be scoped like
+// exampleFunction = (param) => {
+	//code;
+// }
+
+// While React Native functions
+// can look like exampleFunctionRN(param) {
+	//code;
+// }
