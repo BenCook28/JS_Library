e7328667931a341ba0a5294a5c01820578d86af8
Benjamin Cook <benthomascook28@gmail.com>
2018-01-17 22:52:52 -0500

    Added 1.94 Git Bisect

---
+++ 1.94_Git_Bisect.js
@@ -0,0 +1,7 @@
+// If we want to know when something
+// stoped working correctly,
+// in the terminal we can run
+// git bisect start
+// git diff
+// // and then to close we run
+// git bisect resect
