7af2c27d21ba25e30de9a595dfb1a5c09305ff2a
Benjamin Cook <benthomascook28@gmail.com>
2018-01-18 20:56:50 -0500

    Added. 1.95 Scoping ES2015

---
+++ 1.95_Scoping_ES2015_vs_var.js
@@ -0,0 +1,7 @@
+// Variabled defined with
+// const and let are scoped
+// to the function block they
+// appear in, but variables 
+// defined with var can be
+// accessed throughout the
+// entire app.
