91f240bb8148dec239a0adad93b135c0c2989af2
Benjamin Cook <benthomascook28@gmail.com>
2018-01-21 21:25:14 -0500

    Added 1.98 Chronos

---
+++ 1.98_Chronos.js
@@ -0,0 +1,6 @@
+// Chrones can identify dates in various
+// formats.  Speaking of various formats,
+// early in the project it is a good idea
+// to first analyze the data source
+// and identify outliers in the data
+// and the data quality in general.
