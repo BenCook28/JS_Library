acc1ea0d88b67e0a0319948997be9ae97eeb2798
Benjamin Cook <benthomascook28@gmail.com>
2018-01-22 22:06:20 -0500

    Added 1.99 Clearing Terminal History

---
+++ 1.99_Clearing_Terminal_History.js
@@ -0,0 +1,7 @@
+// Entering clear into the terminal
+// only clears the current log values,
+// but doesen't clear the history.
+// So previous logs still appear when
+// we run the code again.
+// clear && printf '\e[3J' clears the 
+// terminal history too.
