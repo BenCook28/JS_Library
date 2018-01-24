a5f2ac64e4711a62c4f581dab786c43b7b6f3804
Benjamin Cook <benthomascook28@gmail.com>
2018-01-24 18:58:10 -0500

    Added 2.01_Migrating_Local_Postgres_to_Heroku

---
+++ 2.01_Migrating_Local_Postgres_to_Heroku.js
@@ -0,0 +1,5 @@
+// When migrating local postgres databases to
+// Heroku, before entering the terminal
+// command for pushing mentioned in the docs,
+// we need to add
+//PGUSER=//Postgres username, PGPASSWORD=//Postgres password
