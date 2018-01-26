b6e478388c175ef0d5ff7826867ccabf70e01ee0
Benjamin Cook <benthomascook28@gmail.com>
2018-01-26 16:10:52 -0500

    Added 2.03 Adding Indexes

---
+++ 2.03_Adding_Indexes.js
@@ -0,0 +1,9 @@
+// Indexes speed up databases
+// buy reducing what the SQL
+// searches.  
+
+// The following example creates
+// an index in a deployed Heroku
+// database: heroku pg:psql HEROKU_POSTGRESQL_BLUE_URL
+
+// CREATE INDEX title_on_links ON links(title);
