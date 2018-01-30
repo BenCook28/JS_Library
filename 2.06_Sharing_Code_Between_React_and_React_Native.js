d882a5977139ecd18d2efe738618cdf4feaac5a8
Benjamin Cook <benthomascook28@gmail.com>
2018-01-29 21:57:31 -0500

    Added 2.06 Sharing Code Between React and React Native

---
+++ 2.06_Sharing_Code_Between_React_and_React_Native.js
@@ -0,0 +1,8 @@
+// In general it makes sense to share business 
+// logic and have a repo for this logic, and 
+// directly copying code between separate repos 
+// can actually make sense since the mobile 
+// version and web version tend to diverge 
+// and companies sometimes prioritize either 
+// the web or mobile version.  With navigation, 
+// sharing view-specific code can cause issues.  
