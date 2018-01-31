dc468b01f8fedac2bb4b30da4d0cba7c2126583f
Benjamin Cook <benthomascook28@gmail.com>
2018-01-30 21:53:55 -0500

    Added 2.07 Algolia for Search

---
+++ 2.07_Algolia_for_Search.js
@@ -0,0 +1,7 @@
+// Algolia is a powerful API
+// for search, which offers
+// suggestions, filters, and
+// blazing speed.  It is free
+// for non-profits, but otherwise
+// expensive.  Some large websites
+// with lots of data use Algolia.
