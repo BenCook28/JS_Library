396fd72a9834c512179d33c4a641f2d93305d698
Benjamin Cook <benthomascook28@gmail.com>
2018-02-03 11:46:18 -0500

    Added 2.10 array.prototype.some

---
+++ 2.10_Array.Prototype.Some.js
@@ -0,0 +1,6 @@
+// the .some() method tests whether
+// at least one element in the array
+// passes the logical test, which
+// comes in handy when we want
+// to work with unique values in an
+// array.
