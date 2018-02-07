43e67a5e10898e92fe61f0b1291dfc93875cced1
Benjamin Cook <benthomascook28@gmail.com>
2018-02-07 07:11:10 -0500

    Added 2.14 Elixir Pipe Operator

---
+++ 2.14_Elixir_Pipe_Operator.js
@@ -0,0 +1,9 @@
+// |> solves the problem that
+// nested functions can be 
+// hard to read at times.
+
+// The pipe operator takes the
+// output of the expression on
+// the left and passes it as the
+// first argument of the expression
+// on the right.
