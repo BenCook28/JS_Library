de12b36162959ce86772fa9dc391d45e28504b84
Benjamin Cook <benthomascook28@gmail.com>
2018-02-09 06:49:58 -0500

    Added 2.16 Comments in Elixir

---
+++ 2.16_Elixir_Comments.js
@@ -0,0 +1,12 @@
+// The # sign before a line of code
+// comments out that line of Elixir code.
+
+// A multiline string works as a multiline
+// comment in Elixir per Silvio Mayolo's 
+// Stack Overflow answer about Elixir comments
+
+// """
+//     a = "old code"
+//     a = "more old code"
+//     """
+//     a = "new code"
