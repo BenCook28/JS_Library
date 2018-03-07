18f716e4d677cfb859937200b0d84b1b4dfc2d3b
Benjamin Cook <benthomascook28@gmail.com>
2018-03-06 19:45:19 -0500

    Added 2.37 Sequelize Op.ne

---
+++ 2.37_Sequelize_Op.ne.js
@@ -0,0 +1,10 @@
+// To return SQL database
+// values which aren't 
+// certain values like 
+// null, we can use 
+// Sequelize's Op.ne
+// as shown below:
+
+// subsection: {
+//     [Op.ne]: null
+// }
