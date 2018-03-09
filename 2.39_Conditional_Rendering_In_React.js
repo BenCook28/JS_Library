1c42bf8d4e96b788b5abbe877c96f10a509e20da
Ben Cook <benthomascook28@gmail.com>
2018-03-08 21:01:31 -0500

    Added 2.39 Conditional Rendering in React

---
+++ 2.39_Conditional_Rendering_In_React.js
@@ -0,0 +1,4 @@
+// To conditionally render JSX in
+// React Native, we can declar a function
+// outside the rendr function which performs
+// logical checks and returns JSX per the found condition.
