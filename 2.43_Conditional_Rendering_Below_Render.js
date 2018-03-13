8d86156eb633fce159f058e11d3f9e558aedeb2d
Ben Cook <benthomascook28@gmail.com>
2018-03-12 21:29:25 -0400

    Added 2.43 Conditional Rendering Below Render in React Native

---
+++ 2.43_Conditional_Rendering_Below_Render.js
@@ -0,0 +1,7 @@
+// In React Native,
+// we can conditionally render
+// below the render() function
+// by first initializing variables,
+// and in the logic following if statements,
+// assigning values to the variables
+// we declared.
