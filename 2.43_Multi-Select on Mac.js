147768676f02d59eee037aa4e90d355ddcdb648f
Benjamin Cook <benthomascook28@gmail.com>
2018-03-16 20:52:29 -0400

    Added 2.43 Multi-word Select on Mac

---
+++ 2.43_Multi-Select on Mac.js
@@ -0,0 +1,7 @@
+// After highlighting a word, 
+// perhaps by double-clicking on
+// it, we can press Command + d
+// to highlight the next instance
+// of that word two.  We can keep
+// selecting subsequent words
+// using that method.
