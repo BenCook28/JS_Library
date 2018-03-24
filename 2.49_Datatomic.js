345d38340309e48ffc24cc021417b01b7c66fbed
Benjamin Cook <benthomascook28@gmail.com>
2018-03-24 13:52:26 -0400

    Added 2.49 Datatomic

---
+++ 2.49_Datatomic.js
@@ -0,0 +1,5 @@
+// Datatomic servers as an alternative
+// to SQL.  Users can query the entire
+// history, future transactions,
+// only query once a condition is met,
+// and construct joins much more easily.
