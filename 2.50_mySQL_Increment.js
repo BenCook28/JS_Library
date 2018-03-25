8933b705cae7208f10ae9f24f32cb671851cfd2f
Benjamin Cook <benthomascook28@gmail.com>
2018-03-25 18:56:43 -0400

    Added 2.50 mySQL Increment

---
+++ 2.50_mySQL_Increment.js
@@ -0,0 +1,5 @@
+// If a PHP email function doesn't work
+// because it says a record which doesn't
+// esist is a duplicate, we can run a 
+// query to increase the increment by one.
+// This skips the "duplicate" record.
