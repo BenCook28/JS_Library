edbd1cec526427331c77d4bc5dc9c8fd73ea59bc
Ben Cook <benthomascook28@gmail.com>
2018-04-04 07:18:25 -0400

    Added 2.55 React Native CFBundleIdentifier Does Not Exist

---
+++ 2.55_React_Native_ CFBundleIdentifier_Does_Not_Exist.js
@@ -0,0 +1,11 @@
+// When we see React Native 
+// error that
+// Entry, ":CFBundleIdentifier", Does Not Exist
+
+// we can run:
+
+// delete ios/ or android/
+// react-native eject
+// react-native-git-upgrade
+// react-native link
+// react-native run-ios
