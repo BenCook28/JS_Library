50aee349baeca47630748e74c55aa1bde1042bf7
Benjamin Cook <benthomascook28@gmail.com>
2018-04-28 12:09:21 -0400

    Added 2.62 Ethereum Development in React

---
+++ 2.62_Ethereum_Development_In_React.js
@@ -0,0 +1,6 @@
+// We can use the Web 3 and remix.ethereum.org tools
+// to write smart contracts on Ethereum.
+
+// When we develop on Ethereum, we need to keep
+// the gas cost of each line of code, and 
+// keep security in mind always.
