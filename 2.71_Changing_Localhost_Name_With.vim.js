dcfb346a4fc5bcee9a6eb8182ff36e9a18af0b35
Benjamin Cook <benthomascook28@gmail.com>
2018-06-22 18:36:16 -0400

    Added 2.71 Changing Localhost Name

---
+++ 2.71_Changing_Localhost_Name_With.vim.js
@@ -0,0 +1,8 @@
+// To change the name of local host locally,
+// we can type the below in a terminal:
+//sudo vim /etc/hosts
+// after entering the admin password:
+//then…move the cursor to the bottom of the file
+//press o
+//type: 127.0.0.1<tab>cirrus.local
+//press esc, then :x
