c7edde1e6483633bdea78bb9d96060be71da4633
Benjamin Cook <benthomascook28@gmail.com>
2018-07-09 21:07:42 -0400

    Added 2.76 For Loops in PHP

---
+++ 2.76_For_Loop_PHP.js
@@ -0,0 +1,9 @@
+// We need to put $ signs below every instance of 
+// the iterating variable in PHP as shown below:
+
+// for ( $x = 0; $x < count($values); $x++) {
+//     if(strcmp($values[$x], "in_office") == 0) {
+//     // if($source == "on") {
+//         $container->add_child($text);  
+//     }
+// }
