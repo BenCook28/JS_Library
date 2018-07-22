5d13cea671f6eb88a1e48a63feacd68dc81c10cb
Benjamin Cook <benthomascook28@gmail.com>
2018-07-22 08:11:18 -0400

    Added 2.81 Nested PHP Closing Tags

---
+++ 2.81_Why_Not_to_Close_Nested_PHP_Files.js
@@ -0,0 +1,5 @@
+// Rendering blank lines can cause
+// the white screen of death in PHP.
+// a closed PHP tag in a PHP file used
+// elsewhere can cause blank lines and 
+// thus the white screen of death.
