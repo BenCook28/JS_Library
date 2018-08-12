60d0ba8f2bb63dd9013c851c225128a6e06d93bc
Benjamin Cook <benthomascook28@gmail.com>
2018-08-11 20:00:39 -0400

    adjusted vim locolhost name, added 2.88 linear regression in Python

--- 2.71_Changing_Localhost_Name_With.vim.js
+++ 2.71_Changing_Localhost_Name_With.vim.js
@@ -1,8 +1,8 @@
 // To change the name of local host locally,
 // we can type the below in a terminal:
-//sudo vim /etc/hosts
+// sudo vim / etc / hosts
 // after entering the admin password:
-//then…move the cursor to the bottom of the file
-//press o
-//type: 127.0.0.1<tab>cirrus.local
-//press esc, then :x
+// then…move the cursor to the bottom of the file
+// press o
+// type: 127.0.0.1 cirrus.local
+// press esc, then : x
